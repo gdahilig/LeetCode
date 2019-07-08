3cf35b5be40ec2044be495696c40806d226ecf3e
Gene Dahilig <geno@dahilig.com>
2019-07-07 17:12:49 -0700

    Adding solution for RLE Iterator

--- LeetCodeSolutions/LeetCodeSolutions/Arrays and Strings/RLEIterator.swift
+++ LeetCodeSolutions/LeetCodeSolutions/Arrays and Strings/RLEIterator.swift
@@ -15,11 +15,57 @@ import Foundation
 class RLE_Iterator : Solution {
     class RLEIterator {
         var rle = [Int]()
+        var pos : Int
         init(_ A: [Int]) {
             self.rle = A
+            self.pos = 0
         }
-        
+        /*
+         Runtime: 20 ms, faster than 100.00% of Swift online submissions for RLE Iterator.
+         Memory Usage: 21.4 MB, less than 33.33% of Swift online submissions for RLE Iterator.
+         
+         Removed in the previous version the deletion of the first two items.  Instead, a position
+         stored for the current item and incremented (by two, one for the number of values, and the other for the value) every time an item was consumed.
+        */
         func next(_ n: Int) -> Int {
+            var count = n
+            
+            while true {
+                if pos >= rle.count {
+                    return -1
+                }
+                if count == 0 {
+                    return rle[pos+1]
+                }
+                // current: 0,x ==> pop
+                if rle[pos] == 0  {
+                    if rle.count <= 2 {
+                        pos += 2
+                        return -1
+                    } else {
+                        pos += 2
+                        continue
+                    }
+                }
+                // rle[0]-count > 0 ==> rle[0] = rle[0]-count
+                if rle[pos] >= count {
+                    rle[pos] = rle[pos] - count
+                    
+                    return rle[pos+1]
+                }
+                // count > rle[0]-count < 0  ==>
+                count = count - rle[pos]
+                pos += 2
+
+            }
+        }
+        /*
+         Success
+         Details
+         Runtime: 24 ms, faster than 89.47% of Swift online submissions for RLE Iterator.
+         Memory Usage: 21.4 MB, less than 33.33% of Swift online submissions for RLE Iterator.
+        */
+        func next_0(_ n: Int) -> Int {
             var count = n
 
             while true {
